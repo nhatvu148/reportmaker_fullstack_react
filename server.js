bf42f34c5dc941d8f914ab99e83ece0be398fcb7
nhatvu148 <54697578+nhatvu148@users.noreply.github.com>
2020-03-09 14:05:50 +0900

    connect to google cloud sql

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db");
 const fs = require("fs");
 const CreateReport = require("./CreateReport");
 const path = require("path");
+require("dotenv").config();
 
 const app = express();
 
@@ -23,17 +24,26 @@ const QUERY_PROJECTS =
 const QUERY_SUBS =
   "SELECT subid, subname_en, subname_jp FROM projectdata.m_submaster";
 
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "123456789",
+//   database: "projectdata"
+// });
+
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "123456789",
-  database: "projectdata"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASS
 });
 
-connection.connect(error => {
-  if (error) {
-    return error;
+connection.connect(err => {
+  if (err) {
+    console.error("Error connecting: " + err.stack);
+    return;
   }
+  console.log("Connected as thread id: " + connection.threadId);
 });
 
 // console.log(connection);
