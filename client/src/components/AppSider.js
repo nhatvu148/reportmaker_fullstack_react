fc6b030bcb90c4e104ccbe54ba6cb8bbb41da361
nhatvu148 <54697578+nhatvu148@users.noreply.github.com>
2020-03-10 11:34:11 +0900

    update appsider

--- client/src/components/AppSider.js
+++ client/src/components/AppSider.js
@@ -20,7 +20,50 @@ const AppSider = props => {
     weeklyReview: { _weeklyReview },
     monthlyReview: { _monthlyReview },
     dailyHistory: { _dailyHistory }
-  } = langContext.currentLangData;
+  } = langContext.currentLangData
+    ? langContext.currentLangData
+    : {
+        inputDailyData: {
+          _inputDailyData: "Input Daily Data",
+          _reportDate: "Report date:",
+          _addARow: "Add a row",
+          _sameAsDate: "Same as date:",
+          _projectId: "Project ID",
+          _projectName: "Project Name",
+          _subId: "Sub ID",
+          _subName: "Sub Name",
+          _startTime: "Start Time",
+          _endTime: "End Time",
+          _workTime: "Work Time",
+          _status: "Status",
+          _comment: "Comment",
+          _totalWorkTime: "Total Work Time",
+          _hours: "hours"
+        },
+        weeklyReview: {
+          _weeklyReview: "Weekly Review"
+        },
+        monthlyReview: {
+          _monthlyReview: "Monthly Review"
+        },
+        dailyHistory: {
+          _dailyHistory: "Daily History",
+          _sortDate: "Sort by date",
+          _date: "Date",
+          _projectId: "Project ID",
+          _projectName: "Project Name",
+          _deadline: "Deadline",
+          _expectedDate: "Expected Date",
+          _subId: "Sub ID",
+          _subName: "Sub Name",
+          _comment: "Comment",
+          _workTime: "Work Time",
+          _startHour: "Start Hour",
+          _startMin: "Start Min",
+          _endHour: "End Hour",
+          _endMin: "End Min"
+        }
+      };
 
   const { selectedKeys, dispatch } = myContext;
 
