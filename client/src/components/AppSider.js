2152839fef244bdccb25348382651a01a50477b6
nhatvu148 <54697578+nhatvu148@users.noreply.github.com>
2020-03-10 11:40:52 +0900

    appsider

--- client/src/components/AppSider.js
+++ client/src/components/AppSider.js
@@ -24,21 +24,7 @@ const AppSider = props => {
     ? langContext.currentLangData
     : {
         inputDailyData: {
-          _inputDailyData: "Input Daily Data",
-          _reportDate: "Report date:",
-          _addARow: "Add a row",
-          _sameAsDate: "Same as date:",
-          _projectId: "Project ID",
-          _projectName: "Project Name",
-          _subId: "Sub ID",
-          _subName: "Sub Name",
-          _startTime: "Start Time",
-          _endTime: "End Time",
-          _workTime: "Work Time",
-          _status: "Status",
-          _comment: "Comment",
-          _totalWorkTime: "Total Work Time",
-          _hours: "hours"
+          _inputDailyData: "Input Daily Data"
         },
         weeklyReview: {
           _weeklyReview: "Weekly Review"
@@ -47,21 +33,7 @@ const AppSider = props => {
           _monthlyReview: "Monthly Review"
         },
         dailyHistory: {
-          _dailyHistory: "Daily History",
-          _sortDate: "Sort by date",
-          _date: "Date",
-          _projectId: "Project ID",
-          _projectName: "Project Name",
-          _deadline: "Deadline",
-          _expectedDate: "Expected Date",
-          _subId: "Sub ID",
-          _subName: "Sub Name",
-          _comment: "Comment",
-          _workTime: "Work Time",
-          _startHour: "Start Hour",
-          _startMin: "Start Min",
-          _endHour: "End Hour",
-          _endMin: "End Min"
+          _dailyHistory: "Daily History"
         }
       };
 
