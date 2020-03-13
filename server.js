078a635d14755e7e5ec621f402cb49f1adbf4981
nhatvu148 <54697578+nhatvu148@users.noreply.github.com>
2020-03-13 21:35:34 +0900

    update npms

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ const db_config = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
-  password: process.env.DB_PASS
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT
 };
 
 let connection;
