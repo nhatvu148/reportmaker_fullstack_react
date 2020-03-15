ddfbcf5ede7d6d02005d689fe28a0b875cb7b06d
nhatvu148 <54697578+nhatvu148@users.noreply.github.com>
2020-03-15 14:17:11 +0900

    revert to 4ce7a23c54e0495dbb3db4a7cb5f5b9b6098916c Creg time sheet

--- client/src/components/WeeklyReview.js
+++ client/src/components/WeeklyReview.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect, useRef } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import MyContext from "../context/table/myContext";
 import AuthContext from "../context/auth/authContext";
 import ProgressBar from "./layout/ProgressBar";
@@ -7,14 +7,11 @@ import { Button, Layout, Breadcrumb, DatePicker, message } from "antd";
 import "antd/dist/antd.css";
 import axios from "axios";
 import moment from "moment";
-import SpreadSheet from "./spreadsheet/SpreadSheet";
 
 const WeeklyReview = props => {
   // console.log(props.match.path);
   const myContext = useContext(MyContext);
   const authContext = useContext(AuthContext);
-  const spreadsheet = useRef();
-
   const { Content } = Layout;
 
   const { loading, dispatch } = myContext;
@@ -23,8 +20,6 @@ const WeeklyReview = props => {
   const name = user && user.name;
 
   const [weekSelect, SetWeekSelect] = useState("");
-  const [sheetEvent, setSheetEvent] = useState("");
-  const [sheet, setSheet] = useState("");
 
   useEffect(() => {
     if (loading) {
@@ -39,74 +34,6 @@ const WeeklyReview = props => {
     dispatch({ type: SELECT_PAGE, payload: "/weeklyreview" });
     // eslint-disable-next-line
   }, []);
-
-  var styledData = {
-    data: [
-      { cell: "a1", value: "Hello" },
-      { cell: "b1", value: "World" },
-      { cell: "c1", value: "Nhat" },
-      { cell: "d1", value: "Vu" },
-      { cell: "e1", value: "Total Price" },
-
-      { cell: "a2", value: "Ecuador" },
-      { cell: "b2", value: "Banana" },
-      { cell: "c2", value: 6.68, css: "someclass" },
-      { cell: "d2", value: 430, css: "someclass" },
-      { cell: "e2", value: 2872.4 }
-    ],
-    styles: {
-      someclass: {
-        background: "#F2F2F2",
-        color: "#F57C00"
-      }
-    }
-  };
-
-  useEffect(() => {
-    console.log(
-      "spreadsheet: ",
-      spreadsheet.current.spreadsheet.events,
-      "sheetEvent: ",
-      sheetEvent
-    );
-    spreadsheet.current.spreadsheet.events.on(
-      "afterValueChange",
-      (cell, value) => {
-        setSheetEvent(`Value in cell ${cell} changed to ${value}`);
-      }
-    );
-    // spreadsheet.current.spreadsheet.setValue("A1", 10);
-  }, [sheetEvent]);
-
-  const parseData = () => {
-    // spreadsheet.current.spreadsheet.parse([
-    //   { cell: "A1", value: 1000 },
-    //   { cell: "B1", value: 5300 },
-    //   { cell: "C1", value: 2900 },
-    //   { cell: "A2", value: 1000 },
-    //   { cell: "B2", value: 5300 },
-    //   { cell: "C2", value: 2900 }
-    // ]);
-    // spreadsheet.current.spreadsheet.load("dataset.json");
-    // spreadsheet.current.spreadsheet.load("", "xlsx");
-    // spreadsheet.current.spreadsheet.export.xlsx(); // export
-    spreadsheet.current.spreadsheet.parse(styledData);
-  };
-
-  const clearAll = () => {
-    spreadsheet.current.spreadsheet.parse([]);
-    setSheetEvent("");
-  };
-
-  const save = () => {
-    console.log(spreadsheet.current.spreadsheet.serialize());
-    setSheet(spreadsheet.current.spreadsheet.serialize());
-  };
-
-  const reload = () => {
-    spreadsheet.current.spreadsheet.parse(sheet);
-    console.log(sheet);
-  };
 
   const onChangeDate = async date => {
     const sunday = date
@@ -165,23 +92,21 @@ const WeeklyReview = props => {
       <Breadcrumb style={{ margin: "16px 0" }} />
       <Content
         style={{
-          display: "flex",
           padding: "20px 50px",
           borderRadius: "2px",
           position: "relative",
-          transition: "all .3s",
-          justifyContent: "center"
+          transition: "all .3s"
         }}
       >
+        <h1>Weekly Review</h1>
+        <DatePicker
+          picker="week"
+          bordered={false}
+          onChange={date => {
+            onChangeDate(date);
+          }}
+        />
         <div>
-          <h1>Weekly Review</h1>
-          <DatePicker
-            picker="week"
-            bordered={true}
-            onChange={date => {
-              onChangeDate(date);
-            }}
-          />
           <Button
             size="large"
             onClick={() => {
@@ -197,18 +122,6 @@ const WeeklyReview = props => {
             Download Report
           </Button>
         </div>
-
-        <SpreadSheet
-          ref={spreadsheet}
-          rowsCount={100}
-          colsCount={20}
-          menu={true}
-          readonly={false}
-        />
-        <button onClick={parseData}>Parse data</button>
-        <button onClick={clearAll}>Clear all</button>
-        <button onClick={save}>Save</button>
-        <button onClick={reload}>Reload</button>
       </Content>
     </Layout>
   );
